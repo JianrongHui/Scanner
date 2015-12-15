831ebc7096d81a94f93f465ba98fee64d8ba9c8b
JianrongHui <hjryxy@gmail.com>
2015-12-15 23:19:48 +0800

    initialize
    initialize

---
+++ Scanner/Scanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scanner
+//
+//  Created by 辉建荣 on 15/12/15.
+//  Copyright © 2015年 JianRongHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scanner/Scanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scanner
+//
+//  Created by 辉建荣 on 15/12/15.
+//  Copyright © 2015年 JianRongHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
